6e2b0c3cdc5c754c0aec1d3dbfd287755eff05c1
nlehel03 <nlehel03@gmail.com>
2024-10-08 21:39:34 +0200

    2024.10.08

---
+++ breakout/Deszka.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace breakout
+{
+    internal class Deszka
+    {
+        public int lepes = 12;
+        public bool balra;
+        public bool jobbra;
+    }
+}
---
+++ breakout/Labda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace breakout
+{
+    public class Labda
+    {
+        public int xAlap = 421;
+        public int yAlap = 500;
+        public int x = 5;
+        public int y = 5;
+        public int xTouchCount = 0;
+        public int yTouchCount = 0;
+        public int lepes = 12;
+    }
+}
